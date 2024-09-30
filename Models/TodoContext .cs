5e8abc16dfa18cca4467d2c36140c55a617b8ab6
Royer Gaétan <royergaetan90@gmail.com>
2024-09-30 23:15:51 +0200

    add todoitem & context (models)

---
+++ Models/TodoContext .cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoNetCore.Models;
+
+public class TodoContext(DbContextOptions<TodoContext> options) : DbContext(options)
+{
+    public DbSet<TodoItem>? TodoItems { get; set; } = null;
+}
+
---
+++ Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoNetCore.Models;
+
+public class TodoItem
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public bool IsComplete { get; set; }
+}
+
